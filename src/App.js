84457eea087159a4ff10d16df092e3a8d53e2032
PatrykDubinski <dubinskipatryk99@gmail.com>
2020-12-21 19:24:45 +0100

    Added Homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import Header from "./components/Header/Header";
 import "./default.scss";
+
+import Header from "./components/Header/Header";
+import Homepage from "./pages/Homepage/Homepage";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <div className="main">
+        <Homepage />
+      </div>
     </div>
   );
 }
